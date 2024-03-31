8cb41884ded680b2119eee7f499956f958d0d277
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-03-31 20:01:01 +0530

    edit user route refactored for resume and avatar

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const connect = async() => {
 }
 
 app.use(cors({
-  origin: ['https://admin.frint.in', 'https://student.frint.in'],
+  origin: ['https://admin.frint.in', 'https://student.frint.in', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(cookieParser())
